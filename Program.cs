7327432a1f311d69fe4074242ada61d61ff016b1
Thiago César <limongreen20@gmail.com>
2022-05-29 21:45:54 -0300

    added feature to delete computer by id

--- Program.cs
+++ Program.cs
@@ -42,6 +42,11 @@ if(modelName == "Computer")
         var computer = computerRepository.GetById(id);
         Console.WriteLine("{0}, {1}, {2}", computer.Id, computer.Ram, computer.Processor);
     }
+     if(modelAction == "Delete"){
+        var id = Convert.ToInt32(args[2]);
+
+        computerRepository.Delete(id);
+    }
 }
 
 
